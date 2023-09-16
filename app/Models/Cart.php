3ab28be00336be6a9347f24136a219be3c2c6e9d
huku3 <mhideyuki7777@icloud.com>
2023-09-16 09:47:05 +0900

    カート機能実装

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,14 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Cart extends Model
 {
     use HasFactory;
-    public function carts()
+
+    public function products()
     {
         return $this->belongsToMany(
-            Cart::class,
+            Product::class,
             'line_items',
-        )->withPivot(['id','quantity']);   
+        )->withPivot(['id','quantity']);
     }
 }
