bdb904b65f74db1ce8bb610e651e880e8a1a8879
hirooooota <140122495+hirooooota@users.noreply.github.com>
2023-09-19 17:51:09 +0900

    Update index.blade.php

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,12 +1,3 @@
-@import 'custom';
-@import "@fortawesome/fontawesome-free/css/all.css";
-
-
-@import 'tailwindcss/base';
-@import 'tailwindcss/components';
-@import 'tailwindcss/utilities';
-
-
 <x-app-layout>
     @section('title')
         カート
