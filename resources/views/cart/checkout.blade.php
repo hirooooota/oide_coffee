8395499611a98ec79c1112c9ab9999f3d9dab4ad
oshibas <saoritsushima@gmail.com>
2023-09-23 14:39:58 +0900

    カート画面の購入ボタンを修正/tail wind

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -1,13 +1,36 @@
-<p>決済ページへリダイレクトします。</p>
-<script src="https://js.stripe.com/v3/"></script>
-<script>
-    const publicKey = '{{ $publicKey }}';
-    const stripe = Stripe(publicKey);
-    window.onload = function() {
-        stripe.redirectToCheckout({
-            sessionId: '{{ $session->id }}'
-        }).then(function(result) {
-            window.location.href = 'http://localhost/cart';
-        });
-    }
-</script>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>決済ページ</title>
+    <!-- Tailwind CSSのリンクを追加 -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-md">
+        <p class="text-2xl mb-4">決済ページへリダイレクトします。</p>
+        <div
+            style="background-color: rgb(251, 255, 0); color: black; padding: 8px 16px; border-radius: 9999px; text-align: center; margin-bottom: 16px;">
+            OOIDE COFFEE
+        </div>
+
+
+        <script src="https://js.stripe.com/v3/"></script>
+        <script>
+            const publicKey = '{{ $publicKey }}';
+            const stripe = Stripe(publicKey);
+            window.onload = function() {
+                stripe.redirectToCheckout({
+                    sessionId: '{{ $session->id }}'
+                }).then(function(result) {
+                    window.location.href = 'http://localhost/cart';
+                });
+            }
+        </script>
+    </div>
+</body>
+
+</html>
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,3 +1,21 @@
+<style>
+    .cart__purchase {
+        background-color: rgb(251, 255, 0);
+        color: black;
+        border-radius: 1rem;
+        transition: background-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+        /* 影を追加 */
+    }
+
+    .cart__purchase:hover {
+        background-color: rgb(255, 255, 0);
+        /* ホバー時の背景色 */
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.4);
+        /* ホバー時の影を変更 */
+    }
+</style>
+
 <x-app-layout>
     @section('title')
         カート
@@ -5,7 +23,7 @@
 
     @section('content')
         <div class="container mx-auto">
-            <div class="cart__title text-2xl font-bold mb-4">ショッピングカート</div>
+            <div class="cart__title text-2xl font-bold mb-4">Shopping Cart</div>
 
             @if (count($line_items) > 0)
                 <div class="cart-wrapper space-y-4">
@@ -30,14 +48,17 @@
                         </div>
                     @endforeach
                 </div>
-                <div class="cart__sub-total mt-4 text-lg font-semibold">
+                <div class="cart__sub-total mt-8 mb-8 text-lg font-semibold text-center">
                     小計：￥{{ number_format($total_price) }}
                 </div>
 
-                <button onClick="location.href='{{ route('cart.checkout') }}'"
-                    class="cart__purchase mt-4 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600">
-                    購入する
-                </button>
+                <div class="text-center"> <!-- 親要素に text-center クラスを追加 -->
+                    <button onClick="location.href='{{ route('cart.checkout') }}'"
+                        class="cart__purchase py-2 px-4 mb-8 text-lg"
+                        style="background-color: rgb(251, 255, 0); color: black; border-radius: 1rem; transition: background-color 0.2s ease-in-out;">
+                        購入する
+                    </button>
+                </div>
             @else
                 <div class="cart__empty text-xl font-semibold mt-4">
                     カートに商品が入っていません。
