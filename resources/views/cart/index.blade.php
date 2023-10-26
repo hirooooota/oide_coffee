013d806558c625ed2a41c15c1419f58acb018d7e
hirooooota <hirooooota@gmail.com>
2023-10-26 10:30:21 +0900

    支払いページに請求先住所の入力欄を追加

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -21,49 +21,48 @@
 
 <x-app-layout>
     @section('title')
-        カート
+    カート
     @endsection
 
     @section('content')
-        <div class="container mx-auto">
-            <div class="cart__title text-2xl font-bold mb-4">Shopping Cart</div>
+    <div class="container mx-auto">
+        <div class="cart__title text-2xl font-bold mb-4">Shopping Cart</div>
 
-            @if (count($line_items) > 0)
-                <div class="cart-wrapper space-y-4">
-                    @foreach ($line_items as $item)
-                        <div class="flex items-center p-4 border border-gray-200 rounded shadow-md">
-                            <img src="{{ asset($item->image) }}" alt="{{ $item->name }}"
-                                class="w-16 h-16 object-cover rounded" />
-                            <div class="ml-4">
-                                <div class="text-lg font-semibold">{{ $item->name }}</div>
-                                <div class="text-gray-600">{{ $item->pivot->quantity }}個</div>
-                                <div class="text-lg font-semibold text-center">
-                                    ￥{{ number_format($item->price * $item->pivot->quantity) }}
-                                </div>
-                                <form method="post" action="{{ route('cart_delete') }}">
-                                    @csrf
-                                    <input type="hidden" name="id" value="{{ $item->pivot->id }}" />
-                                    <button type="submit" class="text-red-600 hover:text-red-800">
-                                        削除
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    @endforeach
+        @if (count($line_items) > 0)
+        <div class="cart-wrapper space-y-4">
+            @foreach ($line_items as $item)
+            <div class="flex items-center p-4 border border-gray-200 rounded shadow-md">
+                <img src="{{ asset($item->image) }}" alt="{{ $item->name }}" class="w-16 h-16 object-cover rounded" />
+                <div class="ml-4">
+                    <div class="text-lg font-semibold">{{ $item->name }}</div>
+                    <div class="text-gray-600">{{ $item->pivot->quantity }}個</div>
+                    <div class="text-lg font-semibold text-center">
+                        ￥{{ number_format($item->price * $item->pivot->quantity) }}
+                    </div>
+                    <form method="post" action="{{ route('cart_delete') }}">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $item->pivot->id }}" />
+                        <button type="submit" class="text-red-600 hover:text-red-800">
+                            削除
+                        </button>
+                    </form>
                 </div>
-                <div class="cart__sub-total mt-8 mb-8 text-lg font-semibold text-center">
-                    小計：￥{{ number_format($total_price) }}
-                </div>
-
-                <div class="text-center">
-                    <button onClick="location.href='{{ route('cart.checkout') }}'" class="custom-button">
-                        購入する
-                    </button>
-                </div>
-            @else
-                <div class="cart__empty text-xl font-semibold mt-4">
-                    カートに商品が入っていません。
-                </div>
-            @endif
+            </div>
+            @endforeach
         </div>
-    </x-app-layout>
+        <div class="cart__sub-total mt-8 mb-8 text-lg font-semibold text-center">
+            小計：￥{{ number_format($total_price) }}
+        </div>
+
+        <div class="text-center">
+            <button onClick="location.href='{{ route('cart.checkout') }}'" class="custom-button">
+                購入する
+            </button>
+        </div>
+        @else
+        <div class="cart__empty text-xl font-semibold mt-4">
+            カートに商品が入っていません。
+        </div>
+        @endif
+    </div>
+</x-app-layout>
